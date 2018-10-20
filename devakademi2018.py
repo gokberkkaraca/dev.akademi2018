3222ef9a7a95882b3f1a8d20378f8ae7422c1c62
Gökberk Karaca <gokberk.karaca@outlook.com>
2018-10-20 16:13:53 +0300

    Improve analysis, change user structure

--- devakademi2018.py
+++ devakademi2018.py
@@ -2,13 +2,20 @@ import json
 from datetime import datetime
 
 # open the file that contains the data
-with open("./all_data 3.json", encoding='utf-8') as file:
-    data = json.loads(file.read())
+with open("./all_data.json", encoding='utf-8') as file:
+    all_data = json.loads(file.read())
 
-users = {}
+with open("./user_data.json", encoding='utf-8') as file:
+    users = json.loads(file.read())
+
+for user in users:
+    user["IMPRESSION"] = [0,0,0,0,0,0,0,0]
+    user["CLICK"] = [0,0,0,0,0,0,0,0]
+
+users = {user["id"]:user for user in users}
 
 # Iterate over all items in data set
-for item in data:
+for item in all_data:
     timestamp = item["event_date"]
     user_id = item["viewer_user_id"]
     event_type = item["event_type"]
@@ -17,25 +24,24 @@ for item in data:
     if timestamp is None or event_type is "" or event_type is None:
         continue
 
-    # Create a key for user if it does not exist
-    if user_id not in users:
-        users[user_id] = {
-            "IMPRESSION": [0,0,0,0],
-            "CLICK": [0,0,0,0]
-        }
-
     # Extract hour from item
     readible_time = str(datetime.fromtimestamp(int(timestamp)))
     hour = (readible_time.split(" ")[1][0:2])
     
     # Add score to time groups of user according to hour
-    if ("00" <= hour < "06"):
+    if ("00" <= hour < "03"):
         users[user_id][event_type][0] = users[user_id][event_type][0] + 1
-    elif ("06" <= hour < "12"):
+    elif ("03" <= hour < "06"):
         users[user_id][event_type][1] = users[user_id][event_type][1] + 1
-    elif ("12" <= hour < "18"):
+    elif ("06" <= hour < "09"):
         users[user_id][event_type][2] = users[user_id][event_type][2] + 1
-    elif ("18" <= hour < "24"):
+    elif ("09" <= hour < "12"):
         users[user_id][event_type][3] = users[user_id][event_type][3] + 1
-
-print(json.dumps(users, indent=True))
+    elif ("12" <= hour < "15"):
+        users[user_id][event_type][4] = users[user_id][event_type][4] + 1
+    elif ("15" <= hour < "18"):
+        users[user_id][event_type][5] = users[user_id][event_type][5] + 1
+    elif ("18" <= hour < "21"):
+        users[user_id][event_type][6] = users[user_id][event_type][6] + 1
+    elif ("21" <= hour < "24"):
+        users[user_id][event_type][7] = users[user_id][event_type][7] + 1
