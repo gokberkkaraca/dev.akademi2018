fc3a0905917c673b37c9054abedacfd5974e6e63
Gökberk Karaca <gokberk.karaca@outlook.com>
2018-10-20 18:09:04 +0300

    Plot click rate depending on time for job feature

--- devakademi2018.py
+++ devakademi2018.py
@@ -48,7 +48,7 @@ for item in all_data:
     elif ("21" <= hour < "24"):
         users[user_id][event_type][7] = users[user_id][event_type][7] + 1
 
-stats_dictionary = {"IMPRESSION": [0,0,0,0,0,0,0,0], "CLICK": [0,0,0,0,0,0,0,0]}
+stats_dictionary = {"RATIO": [0,0,0,0,0,0,0,0]}
 
 jobs = set([user["job"] for uid,user in users.items()])
 jobs = {job:stats_dictionary for job in jobs if job != ''}
@@ -65,41 +65,26 @@ martial_statuses = {martial_status:stats_dictionary for martial_status in martia
 genders = set([user["gender"] for uid,user in users.items()])
 genders = {gender:stats_dictionary for gender in genders if gender != ''}
 
+print(jobs)
+
+click_ratios = {
+    "jobs": {}
+}
 for job in jobs:
     user_group = [user for user in users.values() if user["job"] == job]
+    job_impression = [0,0,0,0,0,0,0,0]
+    job_click = [0,0,0,0,0,0,0,0]
     for user in user_group:
-        jobs[job]["IMPRESSION"] = [x + y for x, y in zip(jobs[job]["IMPRESSION"], user["IMPRESSION"])]
-        jobs[job]["CLICK"] = [x + y for x, y in zip(jobs[job]["CLICK"], user["CLICK"])]
-    jobs[job]["RATIO"] = [x / y if y != 0 else 0 for x, y in zip(jobs[job]["CLICK"], jobs[job]["IMPRESSION"])]
-
-for city in cities:
-    user_group = [user for user in users.values() if user["cityName"] == city]
-    for user in user_group:
-        cities[city]["IMPRESSION"] = [x + y for x, y in zip(cities[city]["IMPRESSION"], user["IMPRESSION"])]
-        cities[city]["CLICK"] = [x + y for x, y in zip(cities[city]["CLICK"], user["CLICK"])]
-    cities[city]["RATIO"] = [x / y if y != 0 else 0 for x, y in zip(cities[city]["CLICK"], cities[city]["IMPRESSION"])]
-
-for education_level in education_levels:
-    user_group = [user for user in users.values() if user["education"] == education_level]
-    for user in user_group:
-        education_levels[education_level]["IMPRESSION"] = [x + y for x, y in zip(education_levels[education_level]["IMPRESSION"], user["IMPRESSION"])]
-        education_levels[education_level]["CLICK"] = [x + y for x, y in zip(education_levels[education_level]["CLICK"], user["CLICK"])]
-    education_levels[education_level]["RATIO"] = [x / y if y != 0 else 0 for x, y in zip(education_levels[education_level]["CLICK"], education_levels[education_level]["IMPRESSION"])]
-
-for gender in genders:
-    user_group = [user for user in users.values() if user["gender"] == gender]
-    for user in user_group:
-        genders[gender]["IMPRESSION"] = [x + y for x, y in zip(genders[gender]["IMPRESSION"], user["IMPRESSION"])]
-        genders[gender]["CLICK"] = [x + y for x, y in zip(genders[gender]["CLICK"], user["CLICK"])]
-    genders[gender]["RATIO"] = [x / y if y != 0 else 0 for x, y in zip(genders[gender]["CLICK"], genders[gender]["IMPRESSION"])]
+        job_impression = [x + y for x, y in zip(user["IMPRESSION"], job_impression)]
+        job_click = [x + y for x, y in zip(user["CLICK"], job_click)]
+    click_ratios["jobs"][job] = [x / y if y != 0 else 0 for x, y in zip(job_click, job_impression)]
 
 plt.figure(figsize=(19.2, 10.8), dpi=100)
 for job in jobs:
     plt.title("Job - Click Time Graph")
     plt.ylabel("Click/Impression Rate")
     plt.xlabel("time")
-    plt.plot(["00-03", "03-06", "06-09", "09-12", "12-15", "15-18", "18-21", "21-24"], jobs[job]["RATIO"], label=job)
-plt.ylim(top=0.025)
+    plt.plot(["00-03", "03-06", "06-09", "09-12", "12-15", "15-18", "18-21", "21-24"], click_ratios["jobs"][job], label=job)
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 pylab.savefig("./plots/" + "jobs" + ".png", bbox_inches="tight")
 plt.clf()
